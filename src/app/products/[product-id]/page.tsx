f0dda8925fca1f5a6eedc59e14d1d39063747120
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-03-20 14:16:17 +0600

    Implement ProductDetailsPage component with product ID retrieval and layout

--- src/app/products/[product-id]/page.tsx
+++ src/app/products/[product-id]/page.tsx
@@ -1,9 +1,20 @@
 /** @format */
 
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>page</div>;
+import React from "react";
+import { useParams } from "next/navigation";
+import { Container } from "@/components/StyledComponents/Container";
+
+const ProductDetailsPage = () => {
+  const params = useParams<{ "product-id": string }>();
+  const productId = Number(params?.["product-id"]);
+
+  return (
+    <Container>
+      <h1>Product Details</h1>
+    </Container>
+  );
 };
 
-export default page;
+export default ProductDetailsPage;
